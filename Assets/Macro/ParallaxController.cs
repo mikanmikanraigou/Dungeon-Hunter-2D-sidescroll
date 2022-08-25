8b88b1697811a856259027ed78ecabbbfa6c6cf3
mikanmikanraigou <50801969+mikanmikanraigou@users.noreply.github.com>
2022-08-25 11:44:00 +0700

    Upload from local

---
+++ Assets/Macro/ParallaxController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+
+    private float length, startPos;
+    public float speed;
+    public GameObject cam;
+    public float parallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length) startPos += length;
+        else if (temp < startPos - length) startPos -= length;
+        
+    }
+
+}
---
+++ Dungeon Hunter v0.5/README.txt
@@ -0,0 +1,9 @@
+Bug yang telah diketahui
+1. Attack Animation saat Jump glitching (damage tetap masuk)
+2. Slide memang belum diimplementasikan (karena keterbatasan waktu)
+3. Spam melee attack terlalu cepat dapat menyebabkan glitching
+
+Bukan bug
+1. Melee attack dan firebolt terlalu cepat (karena tidak diberikan cooldown)
+2. Invisible wall/ground
+3. Death respawn tidak memiliki notifikasi game over
